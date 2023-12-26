a965c291da6217086b0324ed5a3fd932bd3f7d2e
Mohamad Reza <mr.aghalar98@gmail.com>
2023-12-26 13:55:06 +0330

    Ternary

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        float grade = 12.5f;
+        String result = (grade >= 10) ? "Passed" : "Failed";
+//        if (grade >= 10)
+//            result = "Passed";
+//        else
+//            result = "Failed";
+        System.out.println(result);
+    }
+}
